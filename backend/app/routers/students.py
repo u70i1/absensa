3fce76ec94e08e9ca204afc06924aac25f3b78fa
u7o01 <1hasbi.ashshiddiq@gmail.com>
2026-07-31 10:53:39 +0700

    feat: get /students endpoint complete with queries

--- backend/app/routers/students.py
+++ backend/app/routers/students.py
@@ -16,15 +16,28 @@ class FilterParams(BaseModel):
 
     limit: int = Field(10, ge=1, le=100)
     page: int = Field(1, ge=1)
+    name: str = Field(None)
+    class_: str = Field(None, alias="class")
+    nisn: str = Field(None)
 
 
 @router.get("/students", response_model=list[StudentResponse])
 def get_student(
     filter_query: Annotated[FilterParams, Query()], db: Session = Depends(get_db)
 ):
-    # TODO: Add query filters.
+    """Retrieve students with optional queries from the database."""
+    filters = []
+
+    if filter_query.name is not None:
+        filters.append(Student.name.ilike(f"%{filter_query.name}%"))
+    if filter_query.class_ is not None:
+        filters.append(Student.class_.ilike(filter_query.class_))
+    if filter_query.nisn is not None:
+        filters.append(Student.nisn == filter_query.nisn)
+
     stmt = (
         select(Student)
+        .where(*filters)
         .offset((filter_query.page - 1) * filter_query.limit)
         .limit(filter_query.limit)
     )
