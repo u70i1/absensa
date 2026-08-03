6bb5c8acfb705a9bd36a113e8640b36fb5aeaa00
u7o01 <1hasbi.ashshiddiq@gmail.com>
2026-08-03 09:49:07 +0700

    refactor: remove status literal
    Because it is not needed.

--- backend/schemas/Scan.py
+++ backend/schemas/Scan.py
@@ -1,13 +1,10 @@
 from datetime import datetime
-from typing import Literal
 
 from pydantic import BaseModel
 
 
 class ScanResponse(BaseModel):
     """Response schema for POST /scan."""
-
-    # status: Literal["success", "already_scanned"]
     name: str
     class_name: str
     nisn: str
