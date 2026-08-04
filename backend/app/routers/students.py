dba4447f884f465ea42b262b31aa720a51392805
u7o01 <1hasbi.ashshiddiq@gmail.com>
2026-08-04 10:09:17 +0700

    fix: import error

--- backend/app/routers/students.py
+++ backend/app/routers/students.py
@@ -3,7 +3,7 @@ from typing import Annotated
 from app.db.session import get_db
 from app.models.student import Student
 from fastapi import APIRouter, Depends, Query
-from schemas import StudentQuery
+from schemas.StudentQuery import StudentQuery
 from schemas.StudentResponse import StudentResponse
 from sqlalchemy import select
 from sqlalchemy.orm import Session
@@ -13,23 +13,23 @@ router = APIRouter()
 
 @router.get("/students", response_model=list[StudentResponse])
 def get_student(
-    filter_query: Annotated[StudentQuery, Query()], db: Session = Depends(get_db)
+    query: Annotated[StudentQuery, Query()], db: Session = Depends(get_db)
 ):
     """Retrieve students with optional queries from the database."""
     filters = []
 
-    if filter_query.name is not None:
-        filters.append(Student.name.ilike(f"%{filter_query.name}%"))
-    if filter_query.class_ is not None:
-        filters.append(Student.class_.ilike(f"%{filter_query.class_}%"))
-    if filter_query.nisn is not None:
-        filters.append(Student.nisn == filter_query.nisn)
+    if query.name is not None:
+        filters.append(Student.name.ilike(f"%{query.name}%"))
+    if query.class_ is not None:
+        filters.append(Student.class_.ilike(f"%{query.class_}%"))
+    if query.nisn is not None:
+        filters.append(Student.nisn == query.nisn)
 
     stmt = (
         select(Student)
         .where(*filters)
-        .offset((filter_query.page - 1) * filter_query.limit)
-        .limit(filter_query.limit)
+        .offset((query.page - 1) * query.limit)
+        .limit(query.limit)
     )
     students = db.scalars(stmt).all()
 
