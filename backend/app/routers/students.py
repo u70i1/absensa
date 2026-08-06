87112acd0559ecf1deeb150c90744e7e9ac3a18c
u7o01 <1hasbi.ashshiddiq@gmail.com>
2026-08-06 10:15:27 +0700

    feat: add put and delete /students endpoints
    `PUT` updates a student from the students table.
    `DELETE` deletes a student from the students table.

--- backend/app/routers/students.py
+++ backend/app/routers/students.py
@@ -56,3 +56,43 @@ def post_student(payload: StudentRequest, db: Session = Depends(get_db)):
     db.commit()
 
     return new_student
+
+
+@router.put("/students/{student_id}", response_model=StudentResponse)
+def put_student(
+    student_id: int, payload: StudentRequest, db: Session = Depends(get_db)
+):
+    # Student to update must exist in the database
+    to_update = db.get(Student, student_id)
+    if not to_update:
+        raise HTTPException(404)
+
+    # TODO: Prevent duplicate NISN errors on update
+    nisn_dupe_exists = db.scalar(
+        select(Student)
+        .where(Student.id != student_id)
+        .where(Student.nisn == payload.nisn)
+    )
+    if nisn_dupe_exists:
+        raise HTTPException(409)
+
+    to_update.name = payload.name
+    to_update.class_ = payload.class_
+    to_update.current = payload.current
+    to_update.nisn = payload.nisn
+
+    db.commit()
+
+    return to_update
+
+
+
+@router.delete("/students/{student_id}", status_code=204)
+def delete_scan(student_id: int, db: Session = Depends(get_db)):
+    to_delete = db.get(Student, student_id)
+
+    if not to_delete:
+        raise HTTPException(404)
+
+    db.delete(to_delete)
+    db.commit()
