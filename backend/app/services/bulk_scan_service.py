358053dfeef4fe5e086d996330e9a501df7400db
u7o01 <1hasbi.ashshiddiq@gmail.com>
2026-09-03 11:33:26 +0700

    refactor: services for bulk scans

---
+++ backend/app/services/bulk_scan_service.py
@@ -0,0 +1,21 @@
+from app.models.scan_log import ScanLog
+from app.schemas.BulkScanRequest import BulkScanIdOnly
+from sqlalchemy import delete, select
+from sqlalchemy.orm import Session
+
+
+def delete_scans_bulk(db: Session, payload: BulkScanIdOnly):
+    payload_ids = set(payload.ids)
+    if not payload_ids:
+        return
+
+    db_ids = set(db.scalars(select(ScanLog.scan_id)).all())
+
+    db_ids = set(db.scalars(select(ScanLog.scan_id)).all())
+    missing_ids = [i for i in payload_ids if i not in db_ids]
+
+    if missing_ids:
+        return missing_ids
+
+    db.execute(delete(ScanLog).where(ScanLog.scan_id.in_(payload_ids)))
+    return None
